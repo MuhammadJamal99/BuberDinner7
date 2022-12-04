d160e38163fdbb040a7caf68ac85af52c9140488
MuhammadJamal99 <muhammadjammi18@gmail.com>
2022-12-04 22:22:33 +0200

    Add Register Route and Login Route

---
+++ BuberDinner.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.API.Controllers;
+
+[Route("auth")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    [HttpPost("Register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpPost("Login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        return Ok(request);
+    }
+}
